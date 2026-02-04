41ad9bbe042d365cfb76456ad2c95f29fc12217d
gadiel2311-spec <gadiel2311@gmail.com>
2026-02-04 13:34:42 -0500

    Correct path error

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const fs = require("fs");
 
+// BLOCKING, synchronous way
 const texIn = fs.readFileSync("./txt/input.txt", "utf-8");
 console.log(texIn);
+const texOut = `This is what we know about the avocado: ${texIn}.\nCreated on ${Date.now()}`;
+fs.writeFileSync("./txt/output.txt", texOut);
+console.log("File written!");
+
+// NO BLOCKING, synchronous way
+fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
+  console.log(data);
+});
