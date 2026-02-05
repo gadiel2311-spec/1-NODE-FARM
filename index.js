b23e0b142805e23481c0de5d257e8df0e75af7ae
gadiel2311-spec <gadiel2311@gmail.com>
2026-02-05 11:56:17 -0500

    Add new feature2

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const http = require("http");
+const url = require("url");
 
 //////////////////////////////////////////////////////////////
 // FILES
@@ -34,7 +35,13 @@ const http = require("http");
 ////////////////////////////////////////////////////////////////////
 // SERVER//
 const server = http.createServer((req, res) => {
-  res.end("Hello from the server!");
+  const pathName = req.url;
+
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is the OVERVIEW");
+  } else if (pathName === "/product") {
+    res.end("This is the PRODUCT ");
+  }
 });
 /////
 server.listen(8000, "127.0.0.1", () => {
