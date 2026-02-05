883d1b17db99e21ce3fae90a0c945dced1c0c64e
gadiel2311-spec <gadiel2311@gmail.com>
2026-02-05 13:31:52 -0500

    Add validation logic

--- index.js
+++ index.js
@@ -34,6 +34,10 @@ const url = require("url");
 //console.log("Will read file!");
 ////////////////////////////////////////////////////////////////////
 // SERVER//
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
+//
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
@@ -42,12 +46,9 @@ const server = http.createServer((req, res) => {
   } else if (pathName === "/product") {
     res.end("This is the PRODUCT ");
   } else if (pathName === "/api") {
-    ///
-    fs.readFile(`${__dirname}/dev-data/data.json`, "utf-8", (err, data) => {
-      const productData = JSON.parse(data);
-      res.writeHead(200, { "Content-type": "application/json" });
-      res.end(data);
-    });
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
+    //
   } else {
     res.writeHead(404, {
       "Content-type": "text/html",
