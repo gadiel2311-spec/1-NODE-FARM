9cd84b0b130eb5c29d64b057dd70cff4efeb9a35
gadiel2311-spec <gadiel2311@gmail.com>
2026-02-12 13:03:51 -0500

    Fix issues

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
-const slugifuy = require("slugify");
+const slugify = require("slugify");
 const replaceTemplate = require("./modules/replaceTemplate");
 
 //////////////////////////////////////////////////////////////
@@ -36,20 +36,6 @@ const replaceTemplate = require("./modules/replaceTemplate");
 //console.log("Will read file!");
 ////////////////////////////////////////////////////////////////////
 /////////////////// SERVER//
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-
-  if (!product.organic)
-    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-  return output;
-};
 
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
@@ -67,10 +53,14 @@ const tempProduct = fs.readFileSync(
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 //
+
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
+console.log(slugs);
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
+  const { query, pathname } = url.parse(req.url, true);
+
   //////////////////OVERVIEW PAGE//////////////////
-  if (pathName === "/" || pathName === "/overview") {
+  if (pathname === "/" || pathname === "/overview") {
     res.writeHead(200, { "Content-type": "text/html" });
 
     const cardsHtml = dataObj
@@ -81,21 +71,26 @@ const server = http.createServer((req, res) => {
     res.end(output);
 
     //////////////PRODUCT PAGE///////////////
-  } else if (pathName === "/product") {
-    res.end("This is the PRODUCT ");
-    /////////////////API//////////////////////
-  } else if (pathName === "/api") {
-    res.writeHead(200, { "Content-type": "application/json" });
-    res.end(data);
-    ////////////////NOT FOUN
+  } else if (pathname === "/product") {
+    const product = dataObj[query.id];
 
+    /////////////////API//////////////////////
+    //
+
+    res.writeHead(200, { "Content-type": "text/html" });
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
+
+    ////////////////NOT FOUN
+  } else {
     res.writeHead(404, {
       "Content-type": "text/html",
-      "my-own-header": "helo-world",
+      "my-own-header": "hello-world",
     });
     res.end("<h1>Page not found!</h1>");
   }
 });
+
 /////
 server.listen(8000, "127.0.0.1", () => {
   console.log("Listening to request on port 8000");
